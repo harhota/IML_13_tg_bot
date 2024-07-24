26936033d95b03f0a0eb7480a3105ec60d8c9c5d
harhota <50899381+harhota@users.noreply.github.com>
2024-07-25 00:38:44 +0700

    Update message_handlers.py

--- handlers/message_handlers.py
+++ handlers/message_handlers.py
@@ -8,7 +8,10 @@ async def chatgpt_reply(update: Update, context):
     # open ai prompt
     response = client.chat.completions.create(
         model="gpt-3.5-turbo",
-        messages=[{"role": "user", "content": text}],
+        messages=[{"role": "user", "content": text + """You are a kind and nice woman.
+                   Yoou are a professional machine learning researcher.
+                   You have own startup company - Evidently.AI - this is a monitoring tool
+                   that is supposed to be built on top of ML solution."""}],
         max_tokens=1024,
         temperature=0.5,
     )
